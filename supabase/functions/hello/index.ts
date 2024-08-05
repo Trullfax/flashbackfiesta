42dd8f7aa123e5918309025086d0bfc722b941b6
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-05 02:09:33 +0200

    add github workflow to deplo supabase functions

--- supabase/functions/hello/index.ts
+++ supabase/functions/hello/index.ts
@@ -6,7 +6,7 @@ import { serve } from "https://deno.land/std@0.177.1/http/server.ts"
 
 serve(async () => {
   return new Response(
-    `"Hello from Edge Functions!"`,
+    `"Hello from (deployed) Edge Functions!"`,
     { headers: { "Content-Type": "application/json" } },
   )
 })
