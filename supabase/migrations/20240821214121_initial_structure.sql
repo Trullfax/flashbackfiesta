7fb5da04df7da6a7ebfcae672f927e4833db3431
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-22 00:43:22 +0200

    created and synchronized database

---
+++ supabase/migrations/20240821214121_initial_structure.sql
@@ -0,0 +1,15 @@
+grant delete on table "public"."countries" to "postgres";
+
+grant insert on table "public"."countries" to "postgres";
+
+grant references on table "public"."countries" to "postgres";
+
+grant select on table "public"."countries" to "postgres";
+
+grant trigger on table "public"."countries" to "postgres";
+
+grant truncate on table "public"."countries" to "postgres";
+
+grant update on table "public"."countries" to "postgres";
+
+
