c8c8c34f0d86b7d6cb0b32859946c45eb85072df
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-22 16:57:15 +0200

    adding fonts and colours

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      'white': '#ffffff',
+      'black': '#1d1e1d',
+      'grey': '#f2f2f2',
+      'green': '#54e5c4',
+      'red': '#ff847c',
+      'purple': '#c0a7d1',
+      'yellow': '#fff8a0',
+      'blue': '#3fc2e0'
+    },
+    fontFamily: {
+      vampiro: ["Vampiro One", "system-ui"],
+      contrail: ["Contrail One", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
