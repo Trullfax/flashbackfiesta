0808fba7d07f68109fa7e917a3742d1a1a52312a
Anna-Lena <anna-lena.langhans@stud.th-luebeck.de>
2024-08-23 01:14:41 +0200

    added start component

---
+++ webapp/src/lib/components/Start.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import ButtonBig from '$lib/components/ButtonBig.svelte';
+	import logo from '$lib/assets/logo.svg';
+	import TextBlock from '$lib/components/TextBlock.svelte';
+</script>
+
+<div class="flex flex-col items-center gap-10">
+	<img class="max-w-md" src={logo} alt="Flashback Fiesta" />
+	<div class="grid grid-cols-3">
+		<div class="col-start-2 col-end-2 justify-self-center">
+			<ButtonBig text="START GAME" accent_color="#ff847c" />
+		</div>
+		<div class="justify-self-center">
+			<TextBlock
+				text="Dive into the ultimate pop culture challenge with Flashback Fiesta! <br><br> Are you ready to reminisce and rise to the challenge? Join the fiesta and prove your pop culture knowledge in music, games, films and tv shows!"
+			/>
+		</div>
+	</div>
+</div>
---
+++ webapp/src/lib/components/TextBlock.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let text: string;
+</script>
+
+<p class="font-contrail text-lg text-left p-5 bg-white shadow-bold max-w-[25rem]">{@html text}</p>
