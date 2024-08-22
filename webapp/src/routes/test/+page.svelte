e16460027b059168e128c39b1cfbbbf1ee8cea23
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-22 14:22:46 +0200

    moved code to route test

---
+++ webapp/src/routes/test/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	interface Country {
+		name: string;
+	}
+
+	export let data: { countries: Country[]; error?: string };
+</script>
+
+{#if data.error}
+	<p>Error: {data.error}</p>
+{:else}
+	<ul>
+		<h1>Countries</h1>
+		{#each data.countries as country}
+			<li>{country.name}</li>
+		{/each}
+	</ul>
+{/if}
