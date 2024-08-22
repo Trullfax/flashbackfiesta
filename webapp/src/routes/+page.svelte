38b3c3ea7a77f2f5adf78ccd63e189df6d6959ad
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-22 17:59:35 +0200

    tested component and fonts

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import ButtonSmall from '$lib/components/button_small.svelte';
+
 	// Glob import all SVG files from the avatars directory
 	const avatars = import.meta.glob('$lib/assets/avatars/*.svg', { eager: true });
 
@@ -11,14 +13,66 @@
 	const avatarArray: string[] = Object.values(avatars).map((mod) => (mod as SvgModule).default);
 </script>
 
-<h1>Welcome to Flashbackfiesta!</h1>
+<body class="flex flex-col items-center gap-10">
+	<h1>Welcome to Flashbackfiesta!</h1>
 
-<ul>
-	{#each avatarArray as avatar}
-		<li><img src={avatar} alt="Avatar" /></li>
-	{/each}
-</ul>
-#
+	<ul>
+		{#each avatarArray as avatar}
+			<li><img src={avatar} alt="Avatar" /></li>
+		{/each}
+	</ul>
+
+	<ButtonSmall text="LET'S PLAY" />
+
+	<header class="text-center mb-8">
+		<h1 class="text-4xl font-bold font-contrail mb-4 text-black">Color Palette Showcase</h1>
+		<p class="text-lg text-black font-vampiro">
+			This page demonstrates the color palette defined in Tailwind CSS.
+		</p>
+	</header>
+
+	<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-2xl font-semibold text-black">White</h2>
+			<div class="mt-4 h-24 w-full bg-white border-2 border-black rounded"></div>
+		</div>
+
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-2xl font-semibold text-black">Black</h2>
+			<div class="mt-4 h-24 w-full bg-black rounded"></div>
+		</div>
+
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-2xl font-semibold text-black">Grey</h2>
+			<div class="mt-4 h-24 w-full bg-grey rounded"></div>
+		</div>
+
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-2xl font-semibold text-black">Green</h2>
+			<div class="mt-4 h-24 w-full bg-green rounded"></div>
+		</div>
+
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-2xl font-semibold text-black">Red</h2>
+			<div class="mt-4 h-24 w-full bg-red rounded"></div>
+		</div>
+
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-2xl font-semibold text-black">Purple</h2>
+			<div class="mt-4 h-24 w-full bg-purple rounded"></div>
+		</div>
+
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-2xl font-semibold text-black">Yellow</h2>
+			<div class="mt-4 h-24 w-full bg-yellow rounded"></div>
+		</div>
+
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-2xl font-semibold text-black">Blue</h2>
+			<div class="mt-4 h-24 w-full bg-blue rounded"></div>
+		</div>
+	</div>
+</body>
 
 <style>
 	ul {
