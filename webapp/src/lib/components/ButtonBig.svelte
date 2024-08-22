201390f40d020bc845490b97cbf8c152c774e193
Anna-Lena <anna-lena.langhans@stud.th-luebeck.de>
2024-08-23 01:12:25 +0200

    hover effects for buttons

--- webapp/src/lib/components/ButtonBig.svelte
+++ webapp/src/lib/components/ButtonBig.svelte
@@ -3,13 +3,17 @@
 	export let accent_color: string = '#ff847c';
 </script>
 
-<div class="w-[19rem] h-[7rem] relative flex items-center justify-center">
-	<button class="w-[15rem] h-[4rem] flex justify-center items-center bg-yellow z-20 absolute">
-		<p class="font-contrail text-3xl">{text}</p>
+<div class="w-[19rem] h-[7rem] relative flex items-center justify-center group">
+	<button
+		class="w-[15rem] h-[4rem] flex justify-center items-center bg-yellow group-hover:bg-purple transition-all z-20 absolute"
+	>
+		<p class="font-contrail text-3xl group-hover:text-white transition-all">{text}</p>
 	</button>
 	<div
-		class="w-[15rem] h-[4rem] right-12 top-8 border-[0.5rem] z-10 -rotate-[4deg] absolute"
+		class="w-[15rem] h-[4rem] right-12 top-8 border-[0.5rem] z-10 -rotate-[4deg] group-hover:-rotate-[2deg] transition-all absolute"
 		style="border-color: {accent_color}"
 	/>
-	<div class="w-[15rem] h-[4rem] left-12 top-8 border-[0.5rem] z-0 rotate-[2deg] absolute" />
+	<div
+		class="w-[15rem] h-[4rem] left-12 top-8 border-[0.5rem] z-0 rotate-[2deg] group-hover:rotate-[6deg] transition-all absolute"
+	/>
 </div>
