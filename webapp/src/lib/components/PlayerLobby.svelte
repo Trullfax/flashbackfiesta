ee8109daccf0c424d616a57ddf08ec98424bb66b
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-23 23:37:49 +0200

    started to create PlayerLobby Component

---
+++ webapp/src/lib/components/PlayerLobby.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import ButtonBig from '$lib/components/ButtonBig.svelte';
+	import Title from '$lib/components/Title.svelte';
+</script>
+
+<div class="flex flex-col items-center">
+	<Title title="YOUR FIESTA" subtitle="invite your competitors" flip={true} />
+	<div></div>
+	<ButtonBig text="LET'S PLAY" />
+</div>
