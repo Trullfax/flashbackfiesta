270c4b79d5cb017381cc1e7fd09efac0c834c888
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-23 20:57:54 +0200

    added on:click event for future use

--- webapp/src/lib/components/ButtonSmall.svelte
+++ webapp/src/lib/components/ButtonSmall.svelte
@@ -4,12 +4,17 @@
 </script>
 
 <div class="w-[12rem] h-[4rem] relative flex items-center justify-center group">
-	<button class="w-[10rem] h-[2rem] flex justify-center items-center bg-yellow group-hover:bg-purple transition-all z-20 absolute">
+	<button
+		class="w-[10rem] h-[2rem] flex justify-center items-center bg-yellow group-hover:bg-purple transition-all z-20 absolute"
+		on:click
+	>
 		<p class="font-contrail group-hover:text-white transition-all">{text}</p>
 	</button>
 	<div
 		class="w-[10rem] h-[2rem] right-5 top-6 border-[0.4rem] z-10 -rotate-[4deg] group-hover:-rotate-[2deg] transition-all absolute"
 		style="border-color: {accent_color}"
 	/>
-	<div class="w-[10rem] h-[2rem] left-6 top-6 border-[0.4rem] z-0 rotate-[2deg] group-hover:rotate-[4deg] transition-all absolute" />
+	<div
+		class="w-[10rem] h-[2rem] left-6 top-6 border-[0.4rem] z-0 rotate-[2deg] group-hover:rotate-[4deg] transition-all absolute"
+	/>
 </div>
