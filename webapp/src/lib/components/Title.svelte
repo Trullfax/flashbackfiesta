bc2ec70c8bb16014311eb44f55c210caf96d2e74
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-23 20:58:45 +0200

    changed shadow in Title Component

--- webapp/src/lib/components/Title.svelte
+++ webapp/src/lib/components/Title.svelte
@@ -8,12 +8,10 @@
 
 <div class="w-[30rem] h-[15rem] relative">
 	<div class="font-contrail top-[3rem] left-[2.25rem] absolute">
-		<p class="text-[5rem] text-yellow z-30 -rotate-[5deg] absolute">{title}</p>
-		<p class="text-[5rem] text-black z-20 -rotate-[5deg] left-1.5 top-1 absolute">{title}</p>
+		<p class="text-[5rem] text-yellow z-30 -rotate-[5deg] drop-shadow-title absolute">{title}</p>
 	</div>
 	<div class="w-[25rem] top-[7.25rem] left-[7.5rem] font-vampiro absolute">
-		<p class="text-white text-[2.2rem] z-50 -rotate-[5deg] absolute">{subtitle}</p>
-		<p class="text-black text-[2.2rem] z-40 -rotate-[5deg] left-[0.3rem] top-[0.2rem] absolute">
+		<p class="text-white text-[2.2rem] z-50 -rotate-[5deg] drop-shadow-subtitle absolute">
 			{subtitle}
 		</p>
 	</div>
