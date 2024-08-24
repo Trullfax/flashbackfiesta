6ef7a3c381a490c9133cfdaaebd2fcdcd2b7e7e0
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-24 13:44:51 +0200

    added playerName to gameStore

--- webapp/src/lib/stores/gameStore.ts
+++ webapp/src/lib/stores/gameStore.ts
@@ -3,3 +3,4 @@ import { writable } from "svelte/store";
 export const selectedCategory = writable<string>("");
 export const isCreator = writable<boolean>(false);
 export const selectedAvatar = writable<string>("");
+export const playerName = writable<string>("");
