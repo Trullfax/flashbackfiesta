0f3ad6cef494d992bcbc38962592940728b0663e
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-26 00:10:36 +0200

    added frontend logic to load categories and create a game

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,20 +1,32 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { goto } from '$app/navigation';
 
 	import Start from '$lib/components/Start.svelte';
 	import CategorySelection from '$lib/components/CategorySelection.svelte';
 
 	export let data: PageData;
-	console.log(data);
 
 	function scrollToCategorySelection() {
 		document.getElementById('category-section')?.scrollIntoView({ behavior: 'smooth' });
 	}
 
 	function handleCategorySubmit(event: Event) {
-		const { selectedCategory } = (event as CustomEvent<{ selectedCategory: string }>).detail;
-		console.log('Selected Category:', selectedCategory);
-		// Handle further logic, like navigation or API calls
+		const { selectedCategory } = (event as CustomEvent<{ selectedCategory: Partial<Category> }>)
+			.detail;
+
+		createGameAndNavigate(selectedCategory.id ?? '');
+	}
+
+	async function createGameAndNavigate(category_id: string) {
+		const response = await fetch('/', {
+			method: 'POST',
+			body: JSON.stringify({ category_id }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		goto('/setup/' + (await response.json()).game_id);
 	}
 </script>
 
