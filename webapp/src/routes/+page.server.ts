bd0e827f2cecf8dec292ddc14fe51122810ed45f
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-26 00:09:11 +0200

    added load function for homepage to fetch categories

--- webapp/src/routes/+page.server.ts
+++ webapp/src/routes/+page.server.ts
@@ -1,11 +1,11 @@
-import { supabase } from "$lib/supabaseClient";
+import { supabase } from "$lib/supabaseClient.js";
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
     const { data, error } = await supabase.from("Category").select('id, name, picture_path');
 
     if (error) {
-        console.error('Error fetching countries:', error);
+        console.error('Error fetching categories:', error);
         return {
         categories: [] as Partial<Category>[],
         error: error.message
