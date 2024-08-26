2ffc50eac5484cfb7ba3ae5b674ab16caebe249f
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-26 14:26:27 +0200

    refactored error handling inside load functions

--- webapp/src/routes/setup/[gameId]/+page.server.ts
+++ webapp/src/routes/setup/[gameId]/+page.server.ts
@@ -2,27 +2,27 @@ import { supabase } from "$lib/supabaseClient";
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (request) => {
-    const { gameId } = request.params;
-
-    if (!gameId) {
+    try {
+        const { gameId } = request.params;
+    
+        if (!gameId) {
+            throw new Error("gameIs is invalid");
+        }
+    
+        const { data, error } = await supabase.from("Game").select('Category (id, name, picture_path)').eq('id', gameId);
+    
+        if (error) {
+            throw new Error('Error fetching game:' + error.message);
+        }
+    
         return {
-            game: null,
-            error: 'Game ID is invalid'
+            category: data[0].Category as Partial<Category>,
+            error: null
         };
-    }
-
-    const { data, error } = await supabase.from("Game").select('Category (id, name, picture_path)').eq('id', gameId);
-
-    if (error) {
-        console.error('Error fetching game:', error);
+    } catch (error) {
         return {
-            category: {} as Partial<Category>,
+            category: {},
             error: error.message
         };
     }
-
-    return {
-        category: data[0].Category as Partial<Category>,
-        error: null
-    };
 }
