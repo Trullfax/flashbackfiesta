9682479ab666b46c1db068f96006859650f591e7
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-26 02:28:12 +0200

    added form handling for creating a player and scrolling to lobby

--- webapp/src/routes/setup/[game_id]/+page.svelte
+++ webapp/src/routes/setup/[game_id]/+page.svelte
@@ -1,17 +1,47 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { page } from '$app/stores';
+	import { addToast } from '$lib/stores/toastStore';
 
 	import PlayerSelection from '$lib/components/PlayerSelection.svelte';
 	import PlayerLobby from '$lib/components/PlayerLobby.svelte';
 
 	export let data: PageData;
 
+	const { game_id } = $page.params;
+
 	function handlePlayerSubmit(event: Event) {
-		const { username, selectedAvatar } = (
-			event as CustomEvent<{ username: string; selectedAvatar: string }>
+		const { playername, selectedAvatar } = (
+			event as CustomEvent<{ playername: string; selectedAvatar: string }>
 		).detail;
-		console.log('User:', username, 'Selected Avatar:', selectedAvatar);
-		// Handle further logic, like navigation or API calls
+
+		createPlayerAndScrollToPlayerLobby(playername, selectedAvatar);
+	}
+
+	async function createPlayerAndScrollToPlayerLobby(player_name: string, avatar_path: string) {
+		const response = await fetch('/setup', {
+			method: 'POST',
+			body: JSON.stringify({ game_id, player_name, avatar_path }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		const { player, error } = await response.json();
+
+		if (error) {
+			addToast({ message: error, type: 'error' });
+			return;
+		} else if (!player) {
+			addToast({ message: 'player could not created', type: 'error' });
+			return;
+		}
+
+		// set player_id in internal storage
+		localStorage.setItem('player_id', player.id);
+
+		// scroll to player lobby
+		document.getElementById('playerLobby-section')?.scrollIntoView({ behavior: 'smooth' });
 	}
 </script>
 
@@ -19,7 +49,7 @@
 	<section class="h-screen flex items-center justify-center">
 		<PlayerSelection on:submit={handlePlayerSubmit} category={data.category} />
 	</section>
-	<section id="category-section" class="h-screen flex items-center justify-center">
+	<section id="playerLobby-section" class="h-screen flex items-center justify-center">
 		<PlayerLobby />
 	</section>
 </main>
