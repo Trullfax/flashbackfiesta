1d3695ff28eee0bd53abd417bb3a9239a8a39ad1
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-30 00:08:45 +0200

    added background

--- webapp/src/routes/game/[gameId]/+page.svelte
+++ webapp/src/routes/game/[gameId]/+page.svelte
@@ -48,32 +48,34 @@
 		}
 	}
 
-	function handleCardInsert(payload: { new: any; }) {
-	    console.log('New card received:', payload.new);
-	    const newCard = payload.new;
-	    cards = [...cards, newCard];
+	function handleCardInsert(payload: { new: any }) {
+		console.log('New card received:', payload.new);
+		const newCard = payload.new;
+		cards = [...cards, newCard];
 	}
 
 	onMount(() => {
-	    const subscription = supabase
-	        .channel(gameId)
-	        .on(
-	            'postgres_changes',
-	            { event: 'INSERT', schema: 'public', table: 'Card', filter: `game_id=eq.${gameId}` },
-	            handleCardInsert
-	        )
-	        .subscribe();
-			
-	    return () => {
-	        subscription.unsubscribe();
-	    };
+		const subscription = supabase
+			.channel(gameId)
+			.on(
+				'postgres_changes',
+				{ event: 'INSERT', schema: 'public', table: 'Card', filter: `game_id=eq.${gameId}` },
+				handleCardInsert
+			)
+			.subscribe();
+
+		return () => {
+			subscription.unsubscribe();
+		};
 	});
 
 	// sort cards by year
-	$: {cards.sort((a, b) => Number(a.year) - Number(b.year));}
+	$: {
+		cards.sort((a, b) => Number(a.year) - Number(b.year));
+	}
 </script>
 
-<main class="flex flex-col items-center gap-10">
+<main class="h-screen flex flex-col items-center gap-10 bg-game-background bg-no-repeat bg-cover">
 	<h1>Game Cards</h1>
 	<h2>Category: {data.category.name}</h2>
 
