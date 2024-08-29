5e6a699bbc3b8af9a8fb2dde0b35d056fd32478a
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-29 18:37:40 +0200

    fixed grammar on error page

--- webapp/src/routes/setup/[gameId]/+error.svelte
+++ webapp/src/routes/setup/[gameId]/+error.svelte
@@ -8,7 +8,7 @@
 	<div class="flex flex-col gap-8">
 		<p class="font-contrail text-[5rem] text-yellow -rotate-[5deg] drop-shadow-title">BAZINGA</p>
 		<p class="font-contrail text-[2rem] text-white">
-			the game you tried to join does not exist, or is still running
+			the game you tried to join does not exist or is still running
 		</p>
 	</div>
 </main>
