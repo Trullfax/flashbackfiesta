17d41218996b3d3002f881823f93118c2f68eaa7
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-08-29 02:09:28 +0200

    added generating creatorCode before creating game

--- webapp/src/routes/api/create-game/+server.ts
+++ webapp/src/routes/api/create-game/+server.ts
@@ -10,10 +10,12 @@ export const POST: RequestHandler = async ({ request }) => {
             throw new Error('categoryId is invalid');
         }
 
+        const creatorCode = generateCreatorCode();
+
         const { data, error } = await supabase
         .from('Game')
         .insert([
-        { category_id: categoryId },
+        { category_id: categoryId, creator_code: creatorCode },
         ])
         .select()        
 
@@ -27,8 +29,19 @@ export const POST: RequestHandler = async ({ request }) => {
             throw new Error('created game is invalid');
         }
         
-        return json({status: 'success', gameId: id, error: null});
+        return json({status: 'success', gameId: id, creatorCode: creatorCode, error: null});
     } catch (error) {
-        return json({ status: 'error', gameId: '', error: error.message });
+        return json({ status: 'error', gameId: '', creatorCode: '', error: (error as Error).message });
     } 
 };
+
+function generateCreatorCode() {
+    return uuidv4();
+}
+
+// https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+  }
