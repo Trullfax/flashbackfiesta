eccd5bfcd395d43cd3737d93220d0f72efdf3ac9
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-09-01 13:05:36 +0200

    added realtime structure

--- webapp/src/routes/game/[gameId]/+page.svelte
+++ webapp/src/routes/game/[gameId]/+page.svelte
@@ -3,25 +3,81 @@
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import { supabase } from '$lib/supabaseClient';
+	import { error } from '@sveltejs/kit';
 	import CardComponent from '$lib/components/CardFront.svelte';
 
-	export let data: PageData & { game: Partial<Game>; cards: Card[]; category: Category };
+	export let data: PageData;
 
-	const gameId = $page.params.gameId;
+	const { gameId } = $page.params;
+
 	let cards: Card[] = data.cards || [];
 	let numberOfCards = 10;
-	let error: string | null = null;
+
+	onMount(() => {
+		// TODO: Get this check working
+		if (typeof window !== 'undefined') {
+			const playerId = localStorage.getItem('playerId');
+
+			for (const player of data.players) {
+				if (player.id === playerId) {
+					error(404, 'No matching player found');
+					break;
+				}
+			}
+		}
+
+		const channels = supabase
+			.channel(gameId)
+			.on(
+				'postgres_changes',
+				{ event: 'UPDATE', schema: 'public', filter: `game_id=eq.${gameId}`, table: 'Player' },
+				handlePlayerUpdates
+			)
+			.on(
+				'postgres_changes',
+				{ event: 'UPDATE', schema: 'public', filter: `id=eq.${gameId}`, table: 'Game' },
+				handleGameUpdates
+			)
+			.on(
+				'postgres_changes',
+				{ event: 'UPDATE', schema: 'public', filter: `id=eq.${gameId}`, table: 'Card' },
+				handleCardUpdates
+			)
+			.subscribe();
+
+		return () => {
+			channels.unsubscribe();
+		};
+	});
+
+	function handlePlayerUpdates(payload: { new: any }) {
+		console.log('New card received:', payload.new);
+
+		// TODO: implement update handling
+	}
+
+	function handleGameUpdates(payload: { new: any }) {
+		console.log('New card received:', payload.new);
+
+		// TODO: implement update handling
+	}
+
+	function handleCardUpdates(payload: { new: any }) {
+		console.log('New card received:', payload.new);
+
+		// TODO: implement update handling
+	}
 
 	async function generateCards() {
 		try {
 			// TODO: Add loading toast here.
 			console.log('Generating cards...');
-			const response = await fetch(data.category.api_route, {
+			const response = await fetch(data.category?.api_route ?? '', {
 				method: 'POST',
 				body: JSON.stringify({
 					gameId,
-					categoryId: data.game.category_id,
-					difficulty: data.game.difficulty,
+					categoryId: data.category?.id,
+					difficulty: data.game?.difficulty,
 					numberOfCards
 				}),
 				headers: {
@@ -48,27 +104,6 @@
 		}
 	}
 
-	function handleCardInsert(payload: { new: any }) {
-		console.log('New card received:', payload.new);
-		const newCard = payload.new;
-		cards = [...cards, newCard];
-	}
-
-	onMount(() => {
-		const subscription = supabase
-			.channel(gameId)
-			.on(
-				'postgres_changes',
-				{ event: 'INSERT', schema: 'public', table: 'Card', filter: `game_id=eq.${gameId}` },
-				handleCardInsert
-			)
-			.subscribe();
-
-		return () => {
-			subscription.unsubscribe();
-		};
-	});
-
 	// sort cards by year
 	$: {
 		cards.sort((a, b) => Number(a.year) - Number(b.year));
@@ -77,7 +112,7 @@
 
 <main class="h-screen flex flex-col items-center gap-10 bg-game-background bg-no-repeat bg-cover">
 	<h1>Game Cards</h1>
-	<h2>Category: {data.category.name}</h2>
+	<h2>Category: {data.category?.name}</h2>
 
 	<div>
 		<input type="number" min="1" max="100" bind:value={numberOfCards} class="p-2 border rounded" />
@@ -85,10 +120,6 @@
 			Generate Cards
 		</button>
 	</div>
-
-	{#if error}
-		<p class="text-red-500">{error}</p>
-	{/if}
 
 	{#if cards.length > 0}
 		<ul class="flex flex-row flex-wrap gap-5 mt-5">
@@ -98,7 +129,7 @@
 						title={card.name}
 						subtitle={card.creator}
 						imagePath={card.picture_url}
-						accent_color={data.category.hex_color}
+						accent_color={data.category?.hex_color ?? ''}
 						year={Number(card.year)}
 						revealed={true}
 					/>
