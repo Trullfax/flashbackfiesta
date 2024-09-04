e17c87e9208683f15df4e42339e81fb6ec7cdb55
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-09-04 22:57:15 +0200

    added the option to make the hover color white

--- webapp/src/lib/components/ButtonBig.svelte
+++ webapp/src/lib/components/ButtonBig.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
 	export let text: string;
-	export let accent_color: string = '#ff847c';
+	export let hover_white: boolean = false;
+	export let accent_color: string = 'var(--ff-red)';
 </script>
 
 <div class="w-[19rem] h-[7rem] relative flex items-center justify-center group">
-	<button on:click
-		class="w-[15rem] h-[4rem] flex justify-center items-center bg-yellow group-hover:bg-purple transition-all z-20 absolute"
+	<button
 		on:click
+		class="w-[15rem] h-[4rem] flex justify-center items-center bg-yellow transition-all z-20 absolute {hover_white
+			? 'group-hover:bg-white'
+			: 'group-hover:bg-purple'}"
 	>
-		<p class="font-contrail text-3xl group-hover:text-white transition-all">{text}</p>
+		<p
+			class="font-contrail text-3xl {hover_white
+				? 'group-hover:text-black'
+				: 'group-hover:text-white'} transition-all"
+		>
+			{text}
+		</p>
 	</button>
 	<div
 		class="w-[15rem] h-[4rem] right-10 top-8 border-[0.5rem] z-10 -rotate-[3.8deg] group-hover:-rotate-[7deg] transition-all absolute"
