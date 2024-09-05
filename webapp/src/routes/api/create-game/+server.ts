473df708ccb5b8b862b753e3f46e35bfe578bc37
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-09-05 23:45:07 +0200

    stuff

--- webapp/src/routes/api/create-game/+server.ts
+++ webapp/src/routes/api/create-game/+server.ts
@@ -39,7 +39,6 @@ function generateCreatorCode() {
     return uuidv4();
 }
 
-// https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
 function uuidv4() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
       (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
