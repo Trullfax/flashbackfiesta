93eeda1c8b2003969a73b452af9285a8b4038d56
trullfax <trullfax@gmail.com>
2024-10-09 20:48:07 +0200

    set the timeout for /setup to 15 seconds

--- webapp/src/lib/playerTracking.ts
+++ webapp/src/lib/playerTracking.ts
@@ -3,96 +3,99 @@ import { presenceChannel, onlinePlayers, presenceTimeouts } from '$lib/stores/pl
 import { get } from 'svelte/store'; // Used to get the current value of a store
 
 interface Presence {
-    game: Game;
-    player: Player;
-    presence_ref: string;
+	game: Game;
+	player: Player;
+	presence_ref: string;
 }
 
 export async function joinPresence(player: Player, game: Game) {
-    let currentPresenceChannel = get(presenceChannel);
+	let currentPresenceChannel = get(presenceChannel);
 
-    if (currentPresenceChannel !== null) {
-        handlePlayerOnline(player);
-    } else {
+	if (currentPresenceChannel !== null) {
+		handlePlayerOnline(player);
+	} else {
+		// Create the presence channel
+		currentPresenceChannel = supabase
+			.channel(`presence:${game.id}`)
+			.on('presence', { event: 'sync' }, () => {})
+			.on('presence', { event: 'join' }, ({ newPresences }) =>
+				handlePlayerOnline(newPresences[0].player)
+			)
+			.on('presence', { event: 'leave' }, ({ leftPresences }) =>
+				handlePlayerOffline(leftPresences as Presence[])
+			)
+			.subscribe();
 
-        // Create the presence channel
-        currentPresenceChannel = supabase
-            .channel(`presence:${game.id}`)
-            .on('presence', { event: 'sync' }, () => {})
-            .on('presence', { event: 'join' }, ({ newPresences }) => handlePlayerOnline(newPresences[0].player))
-            .on('presence', { event: 'leave' }, ({ leftPresences }) => handlePlayerOffline(leftPresences as Presence[]))
-            .subscribe();
+		// Update the store with the new presence channel
+		presenceChannel.set(currentPresenceChannel);
 
-        // Update the store with the new presence channel
-        presenceChannel.set(currentPresenceChannel);
-
-        // Join the presence channel with the player ID
-        await currentPresenceChannel.track({ player: player, game: game });
-    }
+		// Join the presence channel with the player ID
+		await currentPresenceChannel.track({ player: player, game: game });
+	}
 }
 
 function handlePlayerOnline(player: Player) {
-    onlinePlayers.update(players => {
-        players[player.id] = true;  // Mark the player as online
-        return players;
-    });
+	onlinePlayers.update((players) => {
+		players[player.id] = true; // Mark the player as online
+		return players;
+	});
 
-    // Clear the timeout for this player if it exists
-    presenceTimeouts.update(timeouts => {
-        if (timeouts[player.id] !== null) {
-            clearTimeout(timeouts[player.id] as NodeJS.Timeout);
-            timeouts[player.id] = null;
-        }
-        return timeouts;
-    });
+	// Clear the timeout for this player if it exists
+	presenceTimeouts.update((timeouts) => {
+		if (timeouts[player.id] !== null) {
+			clearTimeout(timeouts[player.id] as NodeJS.Timeout);
+			timeouts[player.id] = null;
+		}
+		return timeouts;
+	});
 }
 
 async function handlePlayerOffline(leftPresences: Presence[]) {
-    const player = leftPresences[0].player;
-    const game = leftPresences[0].game;
+	const player = leftPresences[0].player;
+	const game = leftPresences[0].game;
 
-    // Mark the player as offline right away
-    onlinePlayers.update(players => {
-        players[player.id] = false;  // Mark the player as offline
-        return players;
-    });
+	// Mark the player as offline right away
+	onlinePlayers.update((players) => {
+		players[player.id] = false; // Mark the player as offline
+		return players;
+	});
 
-    const time: number = game.status === 'not_started' ? 5000 : 15000;
+	const time: number = game.status === 'not_started' ? 15000 : 15000;
 
-    // Delay handling player offline to allow for page transitions/rejoins
-    presenceTimeouts.update(timeouts => {
-        timeouts[player.id] = setTimeout(async () => {
-            // Re-check the player's online status at the time of timeout
-            if (get(onlinePlayers)[player.id]) {
-                return;  // Player rejoined, no need to timeout
-            }
+	// Delay handling player offline to allow for page transitions/rejoins
+	presenceTimeouts.update((timeouts) => {
+		timeouts[player.id] = setTimeout(async () => {
+			// Re-check the player's online status at the time of timeout
+			if (get(onlinePlayers)[player.id]) {
+				return; // Player rejoined, no need to timeout
+			}
 
-            const response = await fetch('/api/player-timeout', {
-                method: 'POST',
-                body: JSON.stringify({ player, gameId: game.id }),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
+			const response = await fetch('/api/player-timeout', {
+				method: 'POST',
+				body: JSON.stringify({ player, gameId: game.id }),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
 
-            const { status, error } = await response.json();
+			const { status, error } = await response.json();
 
-            if (!response.ok || status === 'error') {
-                console.error('Failed to delete player:', error || 'Unknown error');
-            } else {
-                console.warn('Player timed out:', player.name);
-            }
-        }, time);
+			if (!response.ok || status === 'error') {
+				console.error('Failed to delete player:', error || 'Unknown error');
+			} else {
+				console.warn('Player timed out:', player.name);
+			}
+		}, time);
 
-        return timeouts;
-    });
+		return timeouts;
+	});
 }
 
 export function unsubscribePresence() {
-    const currentPresenceChannel = get(presenceChannel);
-    currentPresenceChannel?.unsubscribe();
-    
-    presenceChannel.set(null);
-    onlinePlayers.set({});
-    presenceTimeouts.set({});
+	const currentPresenceChannel = get(presenceChannel);
+	currentPresenceChannel?.unsubscribe();
+
+	presenceChannel.set(null);
+	onlinePlayers.set({});
+	presenceTimeouts.set({});
 }
